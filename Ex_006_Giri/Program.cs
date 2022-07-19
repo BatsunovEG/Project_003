f7257669adfd09220a9c054c0c8475e50cc06a5e
Evgeniy <evgeniybatsunov@gmail.com>
2022-07-19 11:57:39 +0300

    ex_006_giri

---
+++ Ex_006_Giri/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 2;
+int b = 76;
+int c = 8;
+int d = 21;
+int e = 35;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max= ");
+Console.WriteLine(max);
