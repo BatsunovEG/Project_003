d2d03c2ecb93cb04b3b34c2357f43bd3a9c1fce5
Evgeniy <evgeniybatsunov@gmail.com>
2022-07-22 14:39:37 +0300

    add ex_010

--- Ex_009_IntroArray/Program.cs
+++ Ex_009_IntroArray/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int[]array = {14,32,303,45,95,66,87,89,39};
+
+int result = Max(Max(array[0], array[1], array[2]),
+ Max(array[3], array[4], array[5]), 
+ Max(array[6], array[7], array[8]));
+
+ Console.WriteLine(result);
+
+//int max = Max( Max(a1, b1, c1),  Max(a2, b2, c2), Max(a3, b3, c3));
+//Console.WriteLine(max);
