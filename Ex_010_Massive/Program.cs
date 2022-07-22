d5ccddc7662fa8a2710aacf866c7e59b0f9ba1a4
Evgeniy <evgeniybatsunov@gmail.com>
2022-07-22 15:44:47 +0300

    ex_010_END

--- Ex_010_Massive/Program.cs
+++ Ex_010_Massive/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿int[] array = {1,23,43,54,98,34,23,98,65};
+
+int n = array.Length;
+int find = 98;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
